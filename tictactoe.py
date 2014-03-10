4add114fafb06e5ebd681e5d564732be4657f30a
em0flaming0 <rossjosephphillips@gmail.com>
2014-03-10 09:33:58 -0700

    completed win logic

--- tictactoe.py
+++ tictactoe.py
@@ -9,8 +9,36 @@ def CheckMove(row, col, board, valid):
     valid = True
     return valid
  
-def CheckWin():
-  print "\nDEBUG\n"
+def testCheckWin(board):
+  if(board[0][0] and board [0][1] and board[0][2] == "X"):
+    return True
+  else:
+    return False
+  
+
+def CheckWin(board):
+  if (
+    (board[0][0] == "X" and board [0][1] == "X" and board[0][2] == "X") or
+    (board[1][0] == "X" and board [1][1] == "X" and board[1][2] == "X") or
+    (board[2][0] == "X" and board [2][1] == "X" and board[2][2] == "X") or
+    (board[0][0] == "X" and board [1][0] == "X" and board[2][0] == "X") or
+    (board[0][1] == "X" and board [1][1] == "X" and board[2][1] == "X") or
+    (board[0][2] == "X" and board [1][2] == "X" and board[2][2] == "X") or
+    (board[0][0] == "X" and board [1][1] == "X" and board[2][2] == "X") or
+    (board[0][2] == "X" and board [1][1] == "X" and board[2][0] == "X")):
+      return True
+  elif (  
+    (board[0][0] == "O" and board [0][1] == "O" and board[0][2] == "O") or
+    (board[1][0] == "O" and board [1][1] == "O" and board[1][2] == "O") or
+    (board[2][0] == "O" and board [2][1] == "O" and board[2][2] == "O") or
+    (board[0][0] == "O" and board [1][0] == "O" and board[2][0] == "O") or
+    (board[0][1] == "O" and board [1][1] == "O" and board[2][1] == "O") or
+    (board[0][2] == "O" and board [1][2] == "O" and board[2][2] == "O") or
+    (board[0][0] == "O" and board [1][1] == "O" and board[2][2] == "O") or
+    (board[0][2] == "O" and board [1][1] == "O" and board[2][0] == "O")):
+      return True
+  else:
+      return False
 
 def FlipPlayer(bit, mark):
   if bit == 1:
@@ -37,7 +65,7 @@ def Move(bit, mark, board):
       c = raw_input("Select Column: "); col = (int(c))-1 
     valid = CheckMove(row, col, board, valid)
   board[row][col] = mark
-  return bit, mark, board
+  return board
 
 def PrintBoard(board):
   print "%s  |  %s  |  %s" % (board[0][0], board[0][1], board[0][2])
@@ -56,12 +84,11 @@ def main():
   ## main()
   print "\nTic Tac Toe... in Python!\n"  
   while True:
-    player_bit, player_mark, board = Move(player_bit, player_mark, board) 
-    win = CheckWin()
-    if win == True:
-      print player_bit+" Wins!"; break
-    player_bit, player_mark = FlipPlayer(player_bit, player_mark)
+    board = Move(player_bit, player_mark, board) 
+    win = CheckWin(board)
     PrintBoard(board)
-
+    if win == True:
+      print "Player ", player_bit, " Wins!"; break
+    player_bit, player_mark = FlipPlayer(player_bit, player_mark)
 if __name__ == "__main__":
   main()
